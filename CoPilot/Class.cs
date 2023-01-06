bbc355b14849a2460cdd14a70324c5c925e33ab8
Paul <paul.farrand@primetss.com.au>
2023-01-06 14:21:52 +1030

    Initial

---
+++ CoPilot/Class.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace CoPilot
+{
+    public class Class
+    {
+        public int id;
+        public string name;
+        public string description;
+
+        Class() { 
+            
+        }
+
+        int CalculateDaysBetweenDates(
+
+    }
+}
